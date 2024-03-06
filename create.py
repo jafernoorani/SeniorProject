7b8ed00a4889562d21d4568e61ebc0a257057f21
jafernoorani <j_noorani@u.pacific.edu>
2024-03-06 14:25:05 -0800

    fixed database issues

--- create.py
+++ create.py
@@ -1,16 +1,21 @@
 import mysql.connector
 
-db = mysqlconnector.connect(host="database-1.cnq4o0i0yw0j.us-east-2.rds.amazonaws.com",    # your host, usually localhost
-                     user="admin",         # your username
-                     passwd="Seniorproject1",  # your password
-                     db="database-1")        # name of the data base
+# Establishing a connection to the MySQL database
+db = mysql.connector.connect(
+    host="database-1.cnq4o0i0yw0j.us-east-2.rds.amazonaws.com",
+    user="admin",
+    passwd="Seniorproject1",
+    db="database-1"
+)
 
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
+# Creating a cursor object to execute SQL queries
 cur = db.cursor()
 
-# Use all the SQL you like
-cur.execute("CREATE TABLE Test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)")
+# Executing an SQL query to create a table named Test with id and name columns
+cur.execute("CREATE TABLE Test (id INTEGER PRIMARY KEY AUTO_INCREMENT, name TEXT)")
 
+# Committing the changes
+db.commit()
 
+# Closing the database connection
 db.close()
