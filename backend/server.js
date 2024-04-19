759fe43b67ee7e53a0562f4ff265b8e37321e4cb
jafernoorani <j_noorani@u.pacific.edu>
2024-04-19 14:55:50 -0700

    database working maybe?

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const html = require('html');
+
 const mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const path = require('path');
@@ -6,7 +8,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'backend')));
 
 // Create connection to MySQL database
 const db = mysql.createConnection({
@@ -32,24 +34,57 @@ app.use(express.urlencoded({ extended: false }));
 app.post('/saveData', (req, res) => {
     const { fullName, emailAddress, password, username } = req.body;
 
-    // Hash the password for security
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
-        if (err) {
-            res.status(500).send('Error hashing password');
-            throw err;
-        }
-
         // Insert user data into MySQL database
         const sql = 'INSERT INTO patientData (fullName, emailAddress, password, userName) VALUES (?, ?, ?, ?)';
-        db.query(sql, [fullName, emailAddress, hashedPassword, username], (err, result) => {
+        db.query(sql, [fullName, emailAddress, password, username], (err, result) => {
             if (err) {
                 res.status(500).send('Error saving user data');
                 throw err;
             }
             res.status(200).send('User registered successfully');
         });
-    });
 });
+
+
+// // Route to handle form submission
+// app.get('/test', (req, res) => {
+//     res.status(200).send('Yay!');
+// });
+
+
+//get me to this html form
+app.get('/createAccount', (req, res) => {
+    // res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname+ '/createAccount.html');
+});
+
+
+
+app.post('/createAccount', (req, res) => {
+    res.status(200).send(req.body);
+});
+
+
+//get me to this html form
+app.get('/user/:id', (req, res) => {
+    // Insert user data into MySQL database
+    const sql = 'SELECT fullName, emailAddress, password, username FROM patientData WHERE patientID = ?';
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) {
+            res.status(500).send('Error saving user data');
+            throw err;
+            }
+            res.status(200).send(result);
+        });
+});
+
+
+
+//login stuff
+app.post('/loginAccount', (req, res) => {
+    res.status(200).send(req.body);
+});
+
 
 // Start the server
 const port = process.env.PORT || 3000;
